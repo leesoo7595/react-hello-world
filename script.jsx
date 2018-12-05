903ae579252e827d3338a9bb1cf5dc7d8090aa93
leesoo <isoo7510@gmail.com>
2018-12-05 14:40:54 +0900

    npm init & HelloWorld class

--- script.jsx
+++ script.jsx
@@ -1,4 +1,15 @@
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>1. Hello world!</h1>
+        <h1>2. Hello world!</h1>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <HelloWorld/>,
   document.getElementById('content')
 )
