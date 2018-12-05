86f46f51736db2c008770a0bb6124e68723ce637
leesoo <isoo7510@gmail.com>
2018-12-05 11:44:00 +0900

    render react properties

--- props.js
+++ props.js
@@ -2,7 +2,7 @@ class HelloWorld extends React.Component {
   render() {
     return React.createElement(
       'h1',
-      null,
+      this.props, // 모든 속성을 자식 엘리먼트에(h1) 전달(id, title)
       'Hello ' + this.props.languageName + ' world!'
     )
   }
@@ -14,7 +14,7 @@ ReactDOM.render(
     null,
     React.createElement(HelloWorld, {
       id: 'java',
-      languageName: 'JAVA',
+      languageName: 'JAVA', // languageName은 h1의 HTML 표준속성이 아니므로 렌더링하지 않는다.
       title: 'Best Feature of Java is OOP'
     }),
     React.createElement(HelloWorld, {
